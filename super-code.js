b3d91517cc8d12dcbf5cc1a78fc274fba8076d29
Arseniy Bushyn <arseniybushyn@gmail.com>
2017-08-11 18:18:51 +0300

    Add css to mantras

--- super-code.js
+++ super-code.js
@@ -1,3 +1,9 @@
-document.onload(e => {
-    let all = document.querySelectorAll('[style~="value"]')
+document.addEventListener('DOMContentLoaded', e => {
+    let tds = document.getElementsByTagName('td')
+    for (var i = 0; i < tds.length; i++) {
+        tds[i].style.whiteSpace = 'normal'
+    }
+
+    navcontainer.style.border = 'initial'
+    navcontainer.style.margin = 'initial'
 })
